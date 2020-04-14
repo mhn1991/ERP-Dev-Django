a6061ddf30defc583e227141211e9634589df0f4
Mohammad Hossein Nourian <mhn_1991@yahoo.com>
2020-04-14 22:41:12 +0100

    product db changed

--- admin/products/models.py
+++ admin/products/models.py
@@ -20,19 +20,27 @@ class Product(models.Model):
     name              = models.TextField(blank=True,null=True)
     code              = models.CharField(max_length=1024,primary_key=True)
     picture           = models.ImageField(upload_to=upload_location,null=True,blank=True)
-    number            = models.IntegerField()
+    numberInstock     = models.IntegerField(default = 0)
+    numberSold        = models.IntegerField(default = 0)
+    numberVisited     = models.IntegerField(default = 0)
     description       = models.TextField(blank=True,null=True)
     price             = models.DecimalField(decimal_places=2,max_digits=1000)
     summary           = models.TextField(blank=True,null=True)
     category          = models.ForeignKey(Category,on_delete=models.CASCADE)
     Author            = models.TextField(blank=True,null=True)
     edited            = models.DateField(auto_now_add=True)
+    creationTime      = models.TimeField(auto_now=True)
     editor            = models.TextField(blank=True,null=True)
-    discount          = models.IntegerField()
+    discount          = models.IntegerField(default = 0)
     discountStartDate = models.DateField(blank=True,null=True)
+    discountStratTime = models.TimeField(blank=True,null=True)
     discountEndDate   = models.DateField(blank=True,null=True)
-    rates             = models.DecimalField(decimal_places=1,max_digits=2)
+    discountEndTime   = models.TimeField(blank=True,null=True)
     information       = models.ForeignKey(Template,on_delete=models.CASCADE)
+    # these are for casting 
+    firstUnit         = models.DecimalField(decimal_places=1,max_digits=10) 
+    secondUnit        = models.DecimalField(decimal_places=1,max_digits=10)
+    thirdUnit         = models.DecimalField(decimal_places=1,max_digits=10) 
 
     def __str__(self):
         return self.code
@@ -45,6 +53,7 @@ class RateComments(models.Model):
     rate    = models.DecimalField(null=True,decimal_places=1,max_digits=2)
     comment = models.TextField(blank=True,null=True) 
     date    = models.DateField(auto_now_add=True)
+    time    = models.TimeField(auto_now_add=True)
     
 class Images(models.Model):
     code = models.ForeignKey(Product,on_delete=models.CASCADE)
